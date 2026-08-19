ac7c159e43c2a413f8b9d8561d93257d7d3fff83
niharikash9410 <niharikash9410@gmail.com>
2026-08-19 10:12:57 +0530

    commit

---
+++ datatype.js
@@ -0,0 +1,26 @@
+const name="Niharika Sharma";
+console.log(name);
+console.log(typeof (name));
+let age="18";
+console.log(age);
+console.log(typeof(age));
+let ismarried=false;
+console.log(ismarried);
+console.log(typeof(ismarried));
+let adreess=null;
+console.log(adreess);
+console.log(typeof(adreess));
+//alert("This is for Practice");
+//prompt("Enter your name");
+let student={
+    name:"Abhay",
+    age:23,
+    isMarried:true,
+    address:{
+    city:"Agra",
+    state:"UP"
+    }
+};
+console.log(student);
+
+
